6f14934c445e53be19bbdb5c6c0e3e5d4c45da46
Bruno Mello <bruno.guitarpro@gmail.com>
2019-03-26 00:56:41 -0300

    Update MaskedTextFieldDelegate.swift

--- Source/InputMask/InputMask/Classes/View/MaskedTextFieldDelegate.swift
+++ Source/InputMask/InputMask/Classes/View/MaskedTextFieldDelegate.swift
@@ -88,18 +88,10 @@ open class MaskedTextFieldDelegate: NSObject, UITextFieldDelegate {
         super.init()
     }
     
-    public override init() {
-    // Interface Builder support
-    self.primaryMaskFormat = ""
-    self.autocomplete = true
-    self.autocompleteOnFocus = true
-    self.rightToLeft = false
-    self.affineFormats = []
-    self.affinityCalculationStrategy = .wholeString
-    self.customNotations = []
-    self.onMaskedTextChangedCallback = nil
-    super.init()
-}
+    public override convenience init() {
+	        // Interface Builder support
+	        self.init(primaryFormat: "")
+	   }
     
     /**
      Maximal length of the text inside the field.
